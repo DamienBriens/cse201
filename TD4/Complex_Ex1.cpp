18f4d1bb8a62676f3aceddcac75337a4d5dd0f38
damien.briens <damien.briens@polytechnique.edu>
2019-10-10 09:15:15 +0200

    Modified version of Ex1
    Added memer functions to the complex class

--- TD4/Complex_Ex1.cpp
+++ TD4/Complex_Ex1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 
 class Complex {
@@ -16,13 +17,51 @@ private:
     // construct a complex number with given real and imaginary parts
     Complex(double real, double imaginary){
       this->real = real;
+
       this->imaginary = imaginary;
     };
-    ~Complex(){std::cout<<"Destroying number" <<real<<  " + " << imaginary << " * i" << std::endl;
-  };
+    ~Complex(){std::cout<<"Destroying number" <<real<<  " + " << imaginary << " * i" << std::endl;}
 
-int main(){
-  Complex a;
-  Complex b(3.0,0.0);
-  Complex c(1.0,2.0);
+
+    double getReal(){
+      return real;
+    };
+
+    double getImaginary(){
+      return imaginary;
+    };
+
+    void setReal(double re){
+      real = re;
+    };
+
+    void setImaginary(double im){
+      imaginary = im;
+    };
+
+    double magnitude(){
+      return sqrt(pow(real,2) + pow(imaginary,2));
+    };
+
+    void normalize(){
+      double alpha = sqrt(pow(real,2) + pow(imaginary,2));
+      real = real/alpha;
+      imaginary = imaginary/alpha;
+    };
+};
+
+int main() {
+  Complex c1;
+  Complex c2 = Complex(3,4);
+
+  c1.normalize();
+  c1.setReal(c2.getReal());
+  c1.setImaginary(c2.getImaginary());
+
+  std::cout << "c1 magnitude: " << c1.magnitude() << std::endl;
+  c1.normalize();
+
+  std::cout << "c1 is: " << c1.getReal() <<
+    " + " << c1.getImaginary() << " * i" << std::endl;
+  std::cout << "c1 magnitude:" << c1.magnitude() << std::endl;
 }
